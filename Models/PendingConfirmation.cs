5eced3a3321b8014bcbe5b51f3f7e7b3c603ed11
woshicongyun <297797692@qq.com>
2026-08-22 20:10:56 +0800

    feat: 修复 Models/PendingConfirmation.cs 完整内容

--- Models/PendingConfirmation.cs
+++ Models/PendingConfirmation.cs
@@ -1,1 +1,12 @@
-新增待确认模型
+using System;
+using BiliLearn.CSharp.Plugin.Services;
+
+namespace BiliLearn.CSharp.Plugin.Models;
+
+public class PendingConfirmation
+{
+    public string Bvid { get; set; } = "";
+    public KnowledgeEntry OldEntry { get; set; } = null!;
+    public string UserQuery { get; set; } = "";
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+}
