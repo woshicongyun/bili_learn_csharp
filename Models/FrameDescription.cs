72b0bbf455f992b4c6c93d093634f1e0684bd3f0
woshicongyun <297797692@qq.com>
2026-08-22 16:42:16 +0800

    refactor: 添加 Models/FrameDescription.cs

---
+++ Models/FrameDescription.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace BiliLearn.CSharp.Plugin.Models;
+
+/// <summary>
+/// 关键帧描述
+/// </summary>
+public class FrameDescription
+{
+    public string FramePath { get; set; } = "";
+    public string Description { get; set; } = "";
+    public double StartTime { get; set; }
+    public double EndTime { get; set; }
+}
