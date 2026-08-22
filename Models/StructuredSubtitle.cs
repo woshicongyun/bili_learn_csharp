c94417c98f8a417ccb12767fd36bd07625f76126
woshicongyun <297797692@qq.com>
2026-08-22 16:42:20 +0800

    refactor: 添加 Models/StructuredSubtitle.cs

---
+++ Models/StructuredSubtitle.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BiliLearn.CSharp.Plugin.Models;
+
+/// <summary>
+/// 结构化字幕条目
+/// </summary>
+public class SubtitleItem
+{
+    public double From { get; set; }
+    public double To { get; set; }
+    public string Text { get; set; } = "";
+}
+
+/// <summary>
+/// 结构化字幕（含全文）
+/// </summary>
+public class StructuredSubtitle
+{
+    public List<SubtitleItem> Items { get; set; } = new();
+    public string FullText => string.Join(" ", Items.Select(i => i.Text));
+}
