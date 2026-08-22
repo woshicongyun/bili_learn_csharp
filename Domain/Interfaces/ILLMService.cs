809a4754ce4db4d1ece2ca3bda81763e5b554ec0
woshicongyun <297797692@qq.com>
2026-08-22 16:42:12 +0800

    refactor: 添加 Domain/Interfaces/ILLMService.cs

---
+++ Domain/Interfaces/ILLMService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BiliLearn.CSharp.Plugin.Domain.Interfaces;
+
+/// <summary>
+/// LLM服务抽象接口
+/// 对应现有: DeepSeekAI, AlifeLLMAdapter
+/// </summary>
+public interface ILLMService : IDisposable
+{
+    /// <summary>
+    /// 发起单轮对话，返回文本
+    /// </summary>
+    Task<string?> ChatAsync(string prompt, int maxTokens = 2000, double temperature = 0.7, CancellationToken ct = default);
+}
