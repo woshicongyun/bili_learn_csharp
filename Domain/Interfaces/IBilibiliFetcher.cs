2246dad21df23fac40c4de2bcd46e1932c46b560
woshicongyun <297797692@qq.com>
2026-08-22 16:42:10 +0800

    refactor: 添加 Domain/Interfaces/IBilibiliFetcher.cs

---
+++ Domain/Interfaces/IBilibiliFetcher.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BiliLearn.CSharp.Plugin.Models;
+
+namespace BiliLearn.CSharp.Plugin.Domain.Interfaces;
+
+/// <summary>
+/// B站API抽象接口
+/// 对应现有 BilibiliApiService
+/// </summary>
+public interface IBilibiliFetcher : IDisposable
+{
+    /// <summary>
+    /// 验证登录状态
+    /// </summary>
+    Task<LoginStatus> VerifyLoginAsync(CancellationToken ct = default);
+    
+    /// <summary>
+    /// 获取视频信息（含播放地址）
+    /// </summary>
+    Task<VideoInfoResult> GetVideoInfoAsync(string bvid, CancellationToken ct = default);
+    
+    /// <summary>
+    /// 获取字幕JSON字符串
+    /// </summary>
+    Task<string?> GetSubtitleAsync(string bvid, long cid, CancellationToken ct = default);
+    
+    /// <summary>
+    /// 获取推荐视频列表
+    /// </summary>
+    Task<List<RecommendItem>> GetRecommendAsync(int count = 10, CancellationToken ct = default);
+}
